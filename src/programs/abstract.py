9680a3105f9a38849c093f1dfd86f9c6c9ee3fb1
MartaAndres <martaandresarroyo@gmail.com>
2016-10-22 18:46:15 +0200

    Make methods static

--- src/programs/abstract.py
+++ src/programs/abstract.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
 
 class AbstractProgram(object):
-    """ Abstract class for each program that can be saved in a session. """
-    def __init__(self, directory):
-        """ Save the session directory. """
-        self.directory = directory
+    """
+    Abstract class for each program that can be saved in a session.
+    The class and all of its subclasses are going to be static.
+    """
 
-    def names(self):
+    def __init__(self):
+        """ Do not implement, we do not want to instance the classes. """
+        raise NotImplementedError
+
+    @staticmethod
+    def names():
         """ List the code of programs that can be saved with this class. """
         raise NotImplementedError
 
-    def save(self, info):
+    @staticmethod
+    def save(directory, info):
         """ Save the window state given the information. """
         raise NotImplementedError
 
-    def restore(self, filename):
-        """ Restore a window from a file. """
+    @staticmethod
+    def restore(self, directory):
+        """ Restore a window from the information in the directory. """
         raise NotImplementedError
 
+    @staticmethod
     def close(self, info):
-        """ Allow for commands before closing window. Not necessary"""
+        """ Allow for commands before closing window. Not necessary. """
         pass
